5fea9add1be93ea19bf01413335b296ba05cfdbd
alumno <alumno@LAB00PC00>
2019-10-10 22:06:46 -0300

    10-10 22:06

--- Parcial_1/Parcial_1/pedidos.c
+++ Parcial_1/Parcial_1/pedidos.c
@@ -1,3 +1,44 @@
 
 
-imprimirClientesMasRecoleccion();
+int initPedidos(struct ePedidos *aArray, int cantidad)
+{
+	int i;
+	int retorno = -1;
+	if(aArray != NULL && cantidad > 0){
+		for(i=0;i<cantidad;i++){
+			aArray[i].status = STATUS_EMPTY;
+		}
+		retorno = 0;
+	}
+	return retorno;
+}
+int buscarLugarLibrePedidos(struct ePedidos *aArray, int cantidad)
+{
+	int retorno = -1;
+	int i;
+	if(aArray != NULL && cantidad > 0){
+		for(i=0;i<cantidad;i++){
+			if(aArray[i].status == STATUS_EMPTY){
+				retorno = i;
+				break;
+			}
+		}
+	}
+	return retorno;
+}
+
+int altaPedidosPorIdClientes(struct ePedidos *aArray, int cantidad, struct ePedidos buffer,struct eClientes.id item)
+{
+	int retorno = -1;
+	int index;
+	if(aArray != NULL && cantidad > 0)
+	{
+		index = buscarLugarLibrePedidos(aArray,cantidad);
+		if(index >= 0)
+		{
+			aArray[index] = buffer;
+			aArray[index].status = STATUS_NOT_EMPTY;
+			aArray[index].id =
+		}
+	}
+}
--- Parcial_1/Parcial_1/pedidos.h
+++ Parcial_1/Parcial_1/pedidos.h
@@ -1,3 +1,6 @@
+#include "Clientes.h"
+#define STATUS_EMPTY 0
+#define STATUS_NOT_EMPTY 1
 
 struct ePedidos
 {
@@ -8,4 +11,6 @@ struct ePedidos
 	int idClientes;
 }
 
-int initPedidos()
+int initPedidos(struct ePedidos *aArray, int cantidad);
+int buscarLugarLibrePedidos(struct ePedidos *aArray, int cantidad);
+int altaPedidosPorIdClientes(struct ePedidos *aArray, int cantidad, struct eClientes.id item);
